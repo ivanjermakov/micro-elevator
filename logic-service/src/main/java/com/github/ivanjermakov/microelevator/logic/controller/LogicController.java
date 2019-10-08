f6168260df462e61b5c48c07aac74c2ceef31ec7
ivanjermakov <vania6600@gmail.com>
2019-10-08 22:25:11 +0300

    configure endpoints

--- logic-service/src/main/java/com/github/ivanjermakov/microelevator/logic/controller/LogicController.java
+++ logic-service/src/main/java/com/github/ivanjermakov/microelevator/logic/controller/LogicController.java
@@ -1,17 +1,23 @@
 package com.github.ivanjermakov.microelevator.logic.controller;
 
+import com.github.ivanjermakov.microelevator.core.model.Route;
+import com.github.ivanjermakov.microelevator.logic.service.LogicService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.util.List;
-
 @RestController
 public class LogicController {
 
+	private final LogicService logicService;
+
+	public LogicController(LogicService logicService) {
+		this.logicService = logicService;
+	}
+
 	@GetMapping("route")
-	public Flux<List<Integer>> route() {
-		return null;
+	public Flux<Route> route() {
+		return logicService.getRouteProcessor();
 	}
 
 }
